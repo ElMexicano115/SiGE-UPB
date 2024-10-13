68e1aa0ce48d9ac4a24eda411e4ed41f92f56854
JezerSamuel <j3z3rsamuel@gmail.com>
2024-10-13 16:29:44 -0500

    Instalacion de laravel breeze y configuracion basica de adminLTE

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\FormController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,10 +14,18 @@ use App\Http\Controllers\FormController;
 |
 */
 
-/*
 Route::get('/', function () {
-    return view('/Formularios/registro');
+    return view('welcome');
 });
-*/
-Route::redirect('/', '/registro');
-Route::get('/registro', [FormController::class, 'showData'])->name('registro');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
