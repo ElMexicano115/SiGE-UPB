8266a7ab2043ff828c8b977c0bb2cccc15f1e56e
ElMexicano115 <buitron_hau@hotmail.com>
2024-10-12 20:39:31 -0500

    pagina inicial

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FormController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
 Route::get('/', function () {
-    return view('welcome');
+    return view('/Formularios/registro');
 });
+*/
+Route::redirect('/', '/registro');
+Route::get('/registro', [FormController::class, 'showData'])->name('registro');
